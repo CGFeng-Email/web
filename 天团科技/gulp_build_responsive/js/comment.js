0b85bd1b30a89861a10b76fead649e645dda79d8
chen <17520583947@163.com>
2024-12-24 18:37:24 +0800

    更新

--- 天团科技/gulp_build_responsive/js/comment.js
+++ 天团科技/gulp_build_responsive/js/comment.js
@@ -4,3 +4,13 @@ $(function () {
 
   NProgress.done();
 });
+
+// h5导航菜单栏
+$('.header .menu').click(function () {
+  $(this).stop().toggleClass('active');
+  $('.h5_nav').stop().toggleClass('h5_nav_active');
+})
+
+$('.h5_nav .c_nav .li').click(function () {
+  $(this).children('.child_down').stop().slideToggle('.active')
+})
