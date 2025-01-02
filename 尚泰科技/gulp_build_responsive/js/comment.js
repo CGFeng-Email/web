cd8d428021633f76d7f2119c749e729b1e9ea425
chen <17520583947@163.com>
2025-01-02 19:55:43 +0800

    更新

--- 尚泰科技/gulp_build_responsive/js/comment.js
+++ 尚泰科技/gulp_build_responsive/js/comment.js
@@ -4,3 +4,21 @@ $(function () {
 
   NProgress.done();
 });
+
+// h5导航菜单栏
+$('.header .menu').click(function () {
+  $(this).stop().toggleClass('active');
+  $('.h5_nav').stop().toggleClass('h5_nav_active');
+})
+
+$('.h5_nav .c_nav .li').click(function () {
+  $(this).children('.child_down').stop().slideToggle('.active')
+})
+
+$('.menu .category_list .li .lead').click(function () {
+  $(this).next().stop().slideToggle()
+})
+
+$('.advantage .list .li').hover(function () {
+  $(this).children('.lead').stop().slideToggle()
+})
