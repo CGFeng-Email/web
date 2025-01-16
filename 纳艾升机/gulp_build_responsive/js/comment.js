33fc1493436b7891fde007d75a83270b7d97172b
chen <17520583947@163.com>
2025-01-16 18:50:07 +0800

    更新

--- 纳艾升机/gulp_build_responsive/js/comment.js
+++ 纳艾升机/gulp_build_responsive/js/comment.js
@@ -9,7 +9,24 @@ $('.header .language').hover(function () {
   $('.header .language .language_list').stop().slideToggle();
 })
 
-$('.menu').click(function () {
+$('.header .menu').click(function () {
   $(this).stop().toggleClass('on');
   $('.inner_mid').stop().slideToggle();
+})
+
+
+var topDome = document.getElementById('top');
+topDome.addEventListener('click', function () {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+})
+
+$('.news .news_list .col_md').hover(function () {
+  $(this).addClass('active').siblings().removeClass('active')
+})
+
+$('.product .menu .category_list .li').click(function () {
+  $(this).children('.child').stop().slideToggle();
 })
