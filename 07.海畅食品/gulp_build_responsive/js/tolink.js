820ff9c3262ad007fe1e9e0349b1b100ceaa5b30
cgf <1291933022@qq.com>
2025-04-14 10:38:19 +0800

    gx

--- 07.海畅食品/gulp_build_responsive/js/tolink.js
+++ 07.海畅食品/gulp_build_responsive/js/tolink.js
@@ -1,21 +1,21 @@
-const toProduct = document.getElementById('toProduct');
-const toService = document.getElementById('toService');
-const h5_toProduct = document.getElementById('h5_toProduct');
-const h5_toServe = document.getElementById('h5_toServe');
+// const toProduct = document.getElementById('toProduct');
+// const toService = document.getElementById('toService');
+// const h5_toProduct = document.getElementById('h5_toProduct');
+// const h5_toServe = document.getElementById('h5_toServe');
 
-toService.addEventListener('click', () => {
-    window.location.assign('./index.html')
-});
+// toService.addEventListener('click', () => {
+//     window.location.assign('./index.html')
+// });
 
-toProduct.addEventListener('click', () => {
-    window.location.assign('./index.html')
-});
+// toProduct.addEventListener('click', () => {
+//     window.location.assign('./index.html')
+// });
 
-h5_toServe.addEventListener('click', () => {
-    window.location.assign('./index.html')
-});
+// h5_toServe.addEventListener('click', () => {
+//     window.location.assign('./index.html')
+// });
 
-h5_toProduct.addEventListener('click', () => {
-    window.location.assign('./index.html')
-});
+// h5_toProduct.addEventListener('click', () => {
+//     window.location.assign('./index.html')
+// });
 
