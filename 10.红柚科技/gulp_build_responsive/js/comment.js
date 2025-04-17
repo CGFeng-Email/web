a0d579b96d7d667b7ef431f8873a609b14e54690
cgf <1291933022@qq.com>
2025-04-18 00:33:10 +0800

    gx

---
+++ 10.红柚科技/gulp_build_responsive/js/comment.js
@@ -0,0 +1,62 @@
+$(function () {
+  AOS.init({
+    disable: 'mobile',
+    duration: 1000,
+    easing: 'ease-in-out',
+    delay: 300,
+  });
+  NProgress.done();
+});
+
+layui.use(function () {
+  var util = layui.util;
+  util.fixbar({
+    bars: [],
+    on: {},
+  })
+});
+
+
+// 节流函数
+function throttle(func, limit) {
+  let lastFunc;
+  let lastRan;
+  return function () {
+    const context = this;
+    const args = arguments;
+    if (!lastRan) {
+      func.apply(context, args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(function () {
+        if ((Date.now() - lastRan) >= limit) {
+          func.apply(context, args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
+    }
+  }
+}
+
+// 使用节流的滚动事件
+window.addEventListener('scroll', throttle(function () {
+  // 获取垂直滚动位置
+  const scrollY = Math.floor(window.scrollY || window.pageYOffset);
+
+  if (scrollY > 3) {
+    // $('.header').removeClass('header_bottom');
+    $('.header .content').css({
+      background: '#000',
+      padding: 0
+
+    })
+  } else {
+    // $('.header').addClass('header_bottom');
+    $('.header .content').css({
+      background: 'linear-gradient(180deg, rgba(0, 0, 0, .7), transparent)',
+      padding: '0 0 .5rem'
+    })
+
+  }
+}, 200)); // 每200ms最多执行一次
