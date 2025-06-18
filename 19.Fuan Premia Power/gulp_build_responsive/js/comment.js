543098f8d69e0df166df1a79f89a7e4ea0d9eacb
chen <17520583947@163.com>
2025-06-18 19:50:12 +0800

    gx

--- 19.Fuan Premia Power/gulp_build_responsive/js/comment.js
+++ 19.Fuan Premia Power/gulp_build_responsive/js/comment.js
@@ -1,17 +1,14 @@
 $(function () {
+  // // 加載完畢
+  NProgress.done();
+
   AOS.init({
     disable: 'mobile',
     duration: 1000,
     easing: 'ease-in-out',
     delay: 300,
   });
-  NProgress.done();
-});
 
-layui.use(function () {
-  var util = layui.util;
-  util.fixbar({
-    bars: [],
-    on: {},
-  })
-});
+
+  
+})
